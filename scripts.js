7188b4787cab0dd55615419503f7ccd46788cb5d
Kyle Orlinski <orlinskikyle@gmail.com>
2016-08-03 15:32:08 -0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,10 @@
+"use strict"
+$(document).ready(function() {
+    var TicSpace = {};
+    TicSpace = function() {
+        $("#overlay").fadeIn("fast", function() {
+            $("#userlightbox").fadeIn();
+        });
+    }
+   TicSpace();
+});
