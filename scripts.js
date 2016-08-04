67cc1b19ad9d073568ca5beaa25685918f327281
Kyle Orlinski <orlinskikyle@gmail.com>
2016-08-04 15:36:59 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,10 +3,16 @@ $(document).ready(function() {
     var TicSpace = {};
     TicSpace = function() {
         var userPick,
-            ticTacToeBoard = [ [0, 1, 2],
-                               [0, 1, 2],
-                               [0, 1, 2] ];
-        
+            computerPick,
+            ticTacToeBoard = [],
+            innerSelections = document.querySelectorAll(".append"),
+            turnCounter = 0;
+            addSelectionEvents();
+            //push userpick to tictactoe array
+            //append computerchoice to tictactoe board
+            setInterval(function() {
+                computerAI();
+            }, 1000);
         
         
         
@@ -15,16 +21,79 @@ $(document).ready(function() {
         });
         
         $(".firstuserbutton").on("click", function() {
-            setFadeEvents("#overlay", "#userlight", "fadeout");
-            setFadeEvents("#tictactoebox", false, "fadein");
             userPick = "X";
+            computerPick = "O";
+            processFadeEvents();
+            appendChoices();
         });
         $(".seconduserbutton").on("click", function() {
+            userPick = "O";
+            computerPick = "X";
+            processFadeEvents();
+            appendChoices();
+        });
+        function computerAI() {
+            if (turnCounter === 1) {
+                var computerTurn = true;
+                for (var x = 0; x < ticTacToeBoard.length; x++) {
+                    if (computerPick === "O" && ticTacToeBoard[x] !== "X") {
+                        if (computerTurn) {
+                            for (var i = 0; i < innerSelections[i]; i++) {
+                                
+                            }
+                        }
+                        ticTacToeBoard[x] = computerPick;
+                    }
+                    if (computerPick === "X" && ticTacToeBoard[x] !== "O") {
+                        ticTacToeBoard[x] = computerPick;
+                    }
+                    console.log(ticTacToeBoard);
+                }
+                turnCounter = 0;
+            }
+        }
+        function addSelectionEvents() {
+            for (var i = 0; i < innerSelections.length; i++) {
+                
+                innerSelections[i].classList.add(i);
+                ticTacToeBoard.push(i);
+                
+                $(innerSelections[i]).on("click", function() {
+                    if (turnCounter === 0) {
+                        
+                        var get;
+                    
+                        for (var q = 0; q < innerSelections.length; q++) {
+                            if ($(this).hasClass("" + q + "")) {
+                                get = $.grep(this.className.split(" "), function(v, x){
+                                    return v.indexOf(ticTacToeBoard[q]) === 0;
+                                }).join();
+                            }
+                        }
+                        $(this).append("<p>" + userPick + "</p>");
+                        
+                        for (var x = 0; x < ticTacToeBoard.length; x++) {
+                            if (get == ticTacToeBoard[x] ) {
+                                ticTacToeBoard[x] = userPick;
+                             }
+                        }
+                        turnCounter++;
+                    }
+                });
+            }
+            //console.log(ticTacToeBoard);
+        }
+        function appendChoices() {
+            $(".playerchoice").append(userPick).css({color: "aqua",
+                                                     textShadow: "0 0 5px black"});
+            $(".computerchoice").append(computerPick).css({color: "red",
+                                                     textShadow: "0 0 5px black"});
+        }
+        function processFadeEvents() {
             setFadeEvents("#overlay", "#userlight", "fadeout");
             setFadeEvents("#tictactoebox", false, "fadein");
-            userPick = "O";
-        });
-        
+            setFadeEvents("#gamestats", false, "fadein");
+        }
         function setFadeEvents(firstObject, secondObject, fadeinOrfadeOut) {
             if (fadeinOrfadeOut === "fadeout") {
                 $(firstObject).fadeOut("slow", function() {
