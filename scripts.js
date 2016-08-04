e6812c31318aa1e240f6966667deeb9af937f6aa
Kyle Orlinski <orlinskikyle@gmail.com>
2016-08-03 23:35:36 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,9 +2,44 @@
 $(document).ready(function() {
     var TicSpace = {};
     TicSpace = function() {
-        $("#overlay").fadeIn("fast", function() {
-            $("#userlightbox").fadeIn();
+        var userPick,
+            ticTacToeBoard = [ [0, 1, 2],
+                               [0, 1, 2],
+                               [0, 1, 2] ];
+        
+        
+        
+        
+        $("#overlay").fadeIn(0, function() {
+            $("#userlightbox").fadeIn("slow");
         });
+        
+        $(".firstuserbutton").on("click", function() {
+            setFadeEvents("#overlay", "#userlight", "fadeout");
+            setFadeEvents("#tictactoebox", false, "fadein");
+            userPick = "X";
+        });
+        $(".seconduserbutton").on("click", function() {
+            setFadeEvents("#overlay", "#userlight", "fadeout");
+            setFadeEvents("#tictactoebox", false, "fadein");
+            userPick = "O";
+        });
+        
+        function setFadeEvents(firstObject, secondObject, fadeinOrfadeOut) {
+            if (fadeinOrfadeOut === "fadeout") {
+                $(firstObject).fadeOut("slow", function() {
+                    if (secondObject == true) {
+                        $(secondObject).fadeOut("slow");
+                    }
+                });
+            } else {
+                $(firstObject).fadeIn("slow", function() {
+                    if (secondObject == true) {
+                        $(secondObject).fadeIn("slow");
+                    }
+                });  
+            }
+        }
     }
    TicSpace();
 });
